ecb283527f9c9ea2fb48a13a81866ce58df46c8b
unknown <Sala300@TUNDFPCGE7F352.FormacionSena.RED>
2019-05-17 14:08:31 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AppWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int sum2 = 0;
+            for (int i = 0; i < 5; i++)
+            {
+
+
+                string nombre, apellido;
+                int edad;
+                Console.WriteLine("Bienvenido a C#");
+                Console.WriteLine("Digite su Nombre");
+                nombre = Console.ReadLine();
+                Console.WriteLine("Digite su Apellido");
+                apellido = Console.ReadLine();
+                Console.WriteLine("Digite su edad");
+                edad = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Bienvenido: {nombre} {apellido}  Tu edad es: {edad}");
+                if (edad >= 18)
+                {
+                    Console.WriteLine("Puedes ingresar a la fiesta");
+                    sum = sum + 1;
+                }
+                else
+                {
+                    Console.WriteLine("No se te permite ingresar a la fiesta eres menor de edad");
+                    sum2 = sum2 + 1;
+                }
+            }
+            Console.WriteLine($"El número de personas mayores de edad es: {sum}  El número de personas menores de edad es:  {sum2}");
+            Console.ReadLine();
+        }
+    }
+}
